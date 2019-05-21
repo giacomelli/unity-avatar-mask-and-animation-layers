1f267607f39ef842a991972e386b1ca2243408b1
giacomelli <giacomelli@gmail.com>
2019-05-21 11:01:07 -0300

    First commit

---
+++ avatar-mask-complete/Assets/_Tutorial/Scripts/AnimationStateController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimationStateController : MonoBehaviour
+{
+    Animator _anim;
+ 
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    public void ToggleParameter(string name)
+    {
+        _anim.SetBool(name, !_anim.GetBool(name));
+    }
+}
---
+++ avatar-mask-starter/Assets/_Tutorial/Scripts/QuitController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class QuitController : MonoBehaviour
+{   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Q))
+            Application.Quit();
+    }
+}
